6a50b79887daf73d031ec7278a8d895dc398a0fd
razvanr91 <razvanroman1991@gmail.com>
2021-06-18 23:48:16 +0100

    End of section 2

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,11 @@
+using System;
+using static System.Console;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int ID { get; set; }
+        public string UserName { get; set; }
+    }
+}
