0dc262b460178b6a33aa3ee0d0604d88e1867f7e
razvanr91 <razvanroman1991@gmail.com>
2021-06-20 21:37:34 +0100

    End of section 4

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -7,5 +7,7 @@ namespace API.Entities
     {
         public int ID { get; set; }
         public string UserName { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
     }
 }
